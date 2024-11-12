7db2a423e7cb391c393706ea1cf6f20e3bcc0191
Esteban Amores <estebanamoresbarrantes@gmail.com>
2024-11-12 11:32:33 -0600

    Documentación Ingles Clase_Pasajero.py
    Cambio la documentación a español

--- BL/Clase_Pasajero.py
+++ BL/Clase_Pasajero.py
@@ -8,42 +8,42 @@ class Pasajero(Persona):
         self.cantidadPasajeros = cantidadPasajeros
         
     def registrar(self):
-        # Validación de identificación
+         # Validation of identification
         while True:
             self.identificacion = input("Digite el número de identificación del pasajero: ").strip()
             if self.identificacion:
                 break
             else:
                 print("El número de identificación no puede estar vacío o contener solo espacios.")
-        # Validación del nombre
+        # Validation of name
         while True:
             self.nombre = input("Digite el nombre del pasajero: ").strip()
             if self.nombre:
                 break
             else:
                 print("El nombre no puede estar vacío o contener solo espacios.")
-        # Validación del primer apellido
+        # Validation of the first last name
         while True:
             self.apellido1 = input("Digite el primer apellido del pasajero: ").strip()
             if self.apellido1:
                 break
             else:
                 print("El primer apellido no puede estar vacío o contener solo espacios.")
-        # Validación del segundo apellido
+        # Validation of the second last name
         while True:
             self.apellido2 = input("Digite el segundo apellido del pasajero: ").strip()
             if self.apellido2:
                 break
             else:
                 print("El segundo apellido no puede estar vacío o contener solo espacios.")
-        # Validación de la fecha de nacimiento en formato yyyy-MM-dd
+        # Validation of date of birth in yyyy-MM-dd format
         while True:
             self.fechaNacimiento = input("Digite la fecha de nacimiento del pasajero (yyyy-MM-dd): ").strip()
             if re.match(r"^\d{4}-\d{2}-\d{2}$", self.fechaNacimiento):
                 break
             else:
                 print("Formato de fecha no válido. Use el formato yyyy-MM-dd.")
-        # Validación del género
+        # Gender validation
         while True:
             gen = input("""
             Seleccione el género:
@@ -55,7 +55,7 @@ class Pasajero(Persona):
                 break
             else:
                 print("Opción no válida. Ingrese 'F' para Femenino o 'M' para Masculino.")
-        #Valida que sea un dato entero        
+      #Validate that it is an integer data        
         while True:
             try:
                 cantidad_personas = int(input("Digite la cantidad de personas: ").strip())
